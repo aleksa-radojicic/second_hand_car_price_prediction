14403e16741706f09e3366470401ce1f5b4382b7
aleksa-radojicic <aleksa.radojicic@outlook.com>
2024-02-19 20:26:43 +0100

    Added primitive testing of .ipynb and .py versions of cleaning functions

--- src/features/univariate_analysis.py
+++ src/features/univariate_analysis.py
@@ -12,13 +12,14 @@ class UACleaner:
 
     features_info: FeaturesInfo
 
-    def __init__(self, features_info: FeaturesInfo):
+    def __init__(self, features_info: FeaturesInfo = None):
         self.features_info = features_info
 
     @preprocess_init
     def initial_clean(
         self, df: pd.DataFrame, features_info: FeaturesInfo
     ) -> Tuple[pd.DataFrame, FeaturesInfo]:
+        # Transform column type to string
         df.columns = df.columns.astype("string")
 
         # Prefix columns from table general_informations with "gi_"
@@ -29,17 +30,19 @@ class UACleaner:
         columns_from_gi = df.columns[id_1_col_idx + 1 : id_2_col_idx].values
         columns_from_ai = df.columns[id_2_col_idx + 1 :].values
 
+        # Add prefix 'gi_' to columns from table general_informations
         df.rename(
             columns=dict(zip(columns_from_gi, "gi_" + columns_from_gi)), inplace=True
         )
+        # Add prefix 'ai_' to columns from table additional_informations
         df.rename(
             columns=dict(zip(columns_from_ai, "ai_" + columns_from_ai)), inplace=True
         )
 
-        # Removing redundant ids
+        # Remove redundant ids
         del df["id_1"], df["id_2"]
 
-        # Removing gi_fixed_price that is poorly scraped
+        # Remove gi_fixed_price that is poorly scraped
         del df["gi_fixed_price"]
 
         return df, features_info
@@ -48,6 +51,7 @@ class UACleaner:
         """Returns name of the feature from the function that called this one."""
         function_name = inspect.stack()[1].function
         feature_name = function_name[len(self.CF_PREFIX) :]
+
         return feature_name
 
     @preprocess_init
@@ -55,7 +59,10 @@ class UACleaner:
         self, df: pd.DataFrame, features_info: FeaturesInfo
     ) -> Tuple[pd.DataFrame, FeaturesInfo]:
         feature_name = self._get_feature_name()
+
+        # Add 'name' to 'other' features
         features_info["other"].append(feature_name)
+
         return df, features_info
 
     @preprocess_init
@@ -63,7 +70,10 @@ class UACleaner:
         self, df: pd.DataFrame, features_info: FeaturesInfo
     ) -> Tuple[pd.DataFrame, FeaturesInfo]:
         feature_name = self._get_feature_name()
+
+        # Add 'short_url' to 'other' features
         features_info["other"].append(feature_name)
+
         return df, features_info
 
     @preprocess_init
@@ -72,13 +82,63 @@ class UACleaner:
     ) -> Tuple[pd.DataFrame, FeaturesInfo]:
         feature_name = self._get_feature_name()
 
-        # Removing . from values
+        # Remove '.' from values and transform to numerical
         df[feature_name] = pd.to_numeric(
             df[feature_name].str.slice(stop=-1).str.replace(".", ""), errors="raise"
         )
 
-        # Removing cars that had price=1
+        # Remove cars that had price = 1
         df = df[df[feature_name] != 1]
+
+        cars_price_less_than_100 = df.loc[df[feature_name] < 100, feature_name]
+        # Remove cars that had price < 100
+        df = df.drop(cars_price_less_than_100.index, axis=0)
+
+        return df, features_info
+
+    @preprocess_init
+    def cf_listing_followers_no(
+        self, df: pd.DataFrame, features_info: FeaturesInfo
+    ) -> Tuple[pd.DataFrame, FeaturesInfo]:
+        feature_name = self._get_feature_name()
+
+        # Transform to numerical
+        if (df[feature_name] == "").sum() == pd.to_numeric(
+            df[feature_name]
+        ).isna().sum():
+            df[feature_name] = pd.to_numeric(df[feature_name])
+        else:
+            raise ValueError(
+                "There is a listing_followers_no value that is probably incorrectly parsed."
+            )
+
+        # Added 'listing_followers_no' to numerical features
+        features_info["numerical"].append(feature_name)
+
+        return df, features_info
+
+    @preprocess_init
+    def cf_location(
+        self, df: pd.DataFrame, features_info: FeaturesInfo
+    ) -> Tuple[pd.DataFrame, FeaturesInfo]:
+        feature_name = self._get_feature_name()
+
+        # Add 'location' to 'nominal' features
+        features_info["nominal"].append(feature_name)
+
+        return df, features_info
+
+    @preprocess_init
+    def cf_images_no(
+        self, df: pd.DataFrame, features_info: FeaturesInfo
+    ) -> Tuple[pd.DataFrame, FeaturesInfo]:
+        feature_name = self._get_feature_name()
+
+        # Transformed to numerical
+        df[feature_name] = pd.to_numeric(df[feature_name])
+
+        # Add 'images_no' to 'numerical' features
+        features_info["numerical"].append(feature_name)
 
         return df, features_info
 
@@ -89,6 +149,12 @@ class UACleaner:
         df, features_info = self.cf_name(df=df, features_info=features_info)
         df, features_info = self.cf_short_url(df=df, features_info=features_info)
         df, features_info = self.cf_price(df=df, features_info=features_info)
+        df, features_info = self.cf_listing_followers_no(
+            df=df, features_info=features_info
+        )
+        df, features_info = self.cf_location(df=df, features_info=features_info)
+        df, features_info = self.cf_images_no(df=df, features_info=features_info)
+
         return df, features_info
 
     @preprocess_init
