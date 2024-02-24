92ae109ff97689707b4837b525286b884d7a9027
aleksa-radojicic <aleksa.radojicic@outlook.com>
2024-02-24 19:21:33 +0100

    Extended tests in UnivariateAnalysis.ipynb for testing features_info and columns. Added saving artifacts at the end of UA

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,22 +1,25 @@
+import collections
 import inspect
-import os
 import json
+import os
+from typing import Dict, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import pandas as pd
 from IPython.core.display import Markdown
 from IPython.core.display_functions import display
 from scipy import stats
-from typing import Tuple
 
+from src import config
 from src.config import FeaturesInfo
 
 CF_PREFIX = "cf_"
 NB_SUFFIX = "_nb"
 
 STAGES_DICT = [
-    {"name": "1_InitialCleaning", "folder_path": f"{os.getcwd()}/artifacts"},
-    {"name": "2_UnivariateAnalysis", "folder_path": f"{os.getcwd()}/artifacts"},
+    {"name": "1_IC", "folder_path": f"{os.getcwd()}/artifacts"},
+    {"name": "2_UA", "folder_path": f"{os.getcwd()}/artifacts"},
+    {"name": "3_MA", "folder_path": f"{os.getcwd()}/artifacts"},
 ]
 
 
@@ -96,12 +99,25 @@ def get_value_counts_freq_with_perc(df, column):
     return result
 
 
-def save_dataset_and_metadata(
-    file_name: str, path: str, dataset: pd.DataFrame, metadata: FeaturesInfo
+def save_artifacts(
+    file_name: str,
+    path: str,
+    dataset: pd.DataFrame,
+    metadata: FeaturesInfo,
+    cols_nan_strategy: Optional[Dict[str, List[str]]] = None,
+    idx_to_remove: Optional[List[int]] = None,
 ):
     with open(file=f"{path}/{file_name}_features_info.json", mode="w") as file:
-        json.dump(metadata, file)
+        json.dump(metadata, file, indent=4)
     dataset.to_pickle(path=f"{path}/{file_name}_df.pkl")
+
+    if cols_nan_strategy:
+        with open(file=f"{path}/{file_name}_cols_nan_strategy.json", mode="w") as file:
+            json.dump(cols_nan_strategy, file, indent=4)
+
+    if idx_to_remove:
+        with open(file=f"{path}/{file_name}_idx_to_remove.json", mode="w") as file:
+            json.dump(idx_to_remove, file, indent=4)
 
 
 def load_dataset_and_metadata(
@@ -112,3 +128,27 @@ def load_dataset_and_metadata(
         data = pd.read_pickle(f"{path}/{file_name}_df.pkl")
 
         return data, metadata
+
+
+def test_features_info_and_columns(df: pd.DataFrame, features_info: FeaturesInfo):
+    all_feats = []
+    for key in features_info:
+        if key == "features_to_delete":
+            continue
+        all_feats.extend(features_info[key])
+
+    # Get duplicate features
+    dupe_feats = [
+        feat for feat, count in collections.Counter(all_feats).items() if count > 1
+    ]
+    if dupe_feats:
+        raise AssertionError(
+            f"Features info contains following duplicate features: {dupe_feats}"
+        )
+    all_cols = df.drop(config.LABEL, axis=1).columns.tolist()
+
+    not_in_right_msg = f"Features not in right: {set(all_feats) - set(all_cols)}"
+    not_in_left_msg = f"Features not in left: {set(all_cols) - set(all_feats)}"
+    error_msg = f"{not_in_right_msg}\n{not_in_left_msg}"
+
+    assert set(all_feats) == set(all_cols), error_msg
