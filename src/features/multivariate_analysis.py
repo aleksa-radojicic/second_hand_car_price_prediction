7eb450b750a8e417ea016bd3729bd8755fb0dc84
aleksa-radojicic <aleksa.radojicic@outlook.com>
2024-02-26 23:45:44 +0100

    Analyzed oldtimer cars (MultivarA)

--- src/features/multivariate_analysis.py
+++ src/features/multivariate_analysis.py
@@ -65,6 +65,61 @@ class MACleaner:
         return df, features_info, cols_nan_strategy, idx_to_remove
 
     @preprocess_init
+    def ma_high_seats_cars(
+        self,
+        df: pd.DataFrame,
+        features_info: FeaturesInfo,
+        cols_nan_strategy: Dict[str, List[str]],
+        idx_to_remove: List[int],
+    ) -> Tuple[pd.DataFrame, FeaturesInfo, Dict[str, List[str]], List[int]]:
+
+        high_seats_no = 5
+        high_seats = df.loc[df.ai_seats_no > high_seats_no, ["name", "ai_seats_no"]]
+
+        extreme_high_seats_no = 7
+        more_than_7_seats = high_seats[high_seats.ai_seats_no > extreme_high_seats_no]
+
+        # Drop cars where 'ai_seats_no' > {extreme_high_seats_no}
+        df.drop(more_than_7_seats.index, axis=0, inplace=True)
+
+        idx_to_remove.extend(more_than_7_seats.index.tolist())
+
+        return df, features_info, cols_nan_strategy, idx_to_remove
+
+    @preprocess_init
+    def ma_oldtimers(
+        self,
+        df: pd.DataFrame,
+        features_info: FeaturesInfo,
+        cols_nan_strategy: Dict[str, List[str]],
+        idx_to_remove: List[int],
+    ) -> Tuple[pd.DataFrame, FeaturesInfo, Dict[str, List[str]], List[int]]:
+        oldtimers = df.loc[
+            df.o_Oldtimer,
+            [
+                "name",
+                "gi_production_year",
+                "price",
+                "short_url",
+                "gi_kilometerage",
+                "gi_engine_capacity",
+                "gi_engine_power",
+                "ai_seats_no",
+                "o_Restauriran",
+            ],
+        ]
+
+        seats_no = 2
+        oldtimer_2_seats = oldtimers[oldtimers.ai_seats_no == seats_no]
+
+        # Drop oldtimer cars with 'ai_seats_no' = {seats_no}
+        df.drop(oldtimer_2_seats.index, inplace=True)
+
+        idx_to_remove.extend(oldtimer_2_seats.index.tolist())
+
+        return df, features_info, cols_nan_strategy, idx_to_remove
+
+    @preprocess_init
     def clean(self, df: pd.DataFrame) -> pd.DataFrame:
         features_info = self.features_info
         cols_nan_strategy = self.cols_nan_strategy
@@ -85,6 +140,18 @@ class MACleaner:
                 cols_nan_strategy=cols_nan_strategy,
                 idx_to_remove=idx_to_remove,
             )
+        )
+        df, features_info, cols_nan_strategy, idx_to_remove = self.ma_high_seats_cars(
+            df=df,
+            features_info=features_info,
+            cols_nan_strategy=cols_nan_strategy,
+            idx_to_remove=idx_to_remove,
+        )
+        df, features_info, cols_nan_strategy, idx_to_remove = self.ma_oldtimers(
+            df=df,
+            features_info=features_info,
+            cols_nan_strategy=cols_nan_strategy,
+            idx_to_remove=idx_to_remove,
         )
 
         self.features_info = features_info
