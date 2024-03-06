47ec2ad4c7f91a44d240633142708c5db813539e
aleksa-radojicic <aleksa.radojicic@outlook.com>
2024-03-06 13:53:26 +0100

    Added Runner component & changed Model to be a proper wrapper of sklearn's regressors

--- src/main.py
+++ src/main.py
@@ -3,9 +3,8 @@ from sklearn.pipeline import Pipeline
 
 from src.data.make_dataset import DatasetMaker
 from src.features.build_features import FeaturesBuilder
-from src.logger import logging
-from src.models.train_model import METRICS, Evaluator, Model
-from src.utils import Dataset, get_X_set, get_y_set, train_test_split_custom
+from src.models.train_model import MODELS, Model, Runner
+from src.utils import Dataset, train_test_split_custom
 
 
 def main():
@@ -22,25 +21,11 @@ def main():
     df_train_prep: Dataset = pd.DataFrame(preprocess_pipe.fit_transform(df_train))
     df_test_prep: Dataset = pd.DataFrame(preprocess_pipe.transform(df_test))
 
-    X_train = get_X_set(df_train_prep)
-    X_test = get_X_set(df_test_prep)
-    y_train = get_y_set(df_train_prep)
-    y_test = get_y_set(df_test_prep)
+    pipeline = Pipeline([])
 
-    model = Model("ridge")
-    pipeline = Pipeline([("predictor", model.predictor)])
-
-    logging.info(f"Training predictor {model.name}...")
-    pipeline.fit(X_train, y_train.values)
-    logging.info(f"Trained predictor {model.name} successfully.")
-
-    evaluator = Evaluator(METRICS["r2"])
-    logging.info(f"Evaluating predictor {model.name}.")
-    metrics = evaluator.start(pipeline, X_train, y_train, X_test, y_test)
-    logging.info(f"Evaluated predictor {model.name}.")
-
-    print(f"train evaluation metric: {metrics['train']}")
-    print(f"test evaluation metric: {metrics['test']}")
+    model_names = ["ridge", "dt", "dummy_mean", "dummy_median"]
+    models: list[Model] = [model for model_name, model in MODELS.items() if model_name in model_names]
+    Runner(models).start(pipeline, df_train_prep, df_test_prep)
 
 
 if __name__ == "__main__":
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,8 @@
-from typing import Callable, Dict
+import copy
+from typing import Any, Callable, Dict
 
 import xgboost as xgb
-from sklearn.base import RegressorMixin
+from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.dummy import DummyRegressor
 from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
 from sklearn.linear_model import Ridge
@@ -11,30 +12,43 @@ from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeRegressor
 
 from src import config
-from src.utils import Dataset
+from src.logger import logging
+from src.utils import Dataset, get_X_set, get_y_set
 
 
-class Model:
-    _rf_classifier = RandomForestRegressor(n_jobs=-1, random_state=config.RANDOM_SEED)
-    all_predictors: Dict[str, RegressorMixin] = {
-        "dummy_mean": DummyRegressor(strategy="mean"),
-        "dummy_median": DummyRegressor(strategy="median"),
-        "ridge": Ridge(random_state=config.RANDOM_SEED),
-        # "svr": SVR(),
-        "knn": KNeighborsRegressor(n_jobs=-1),
-        "dt": DecisionTreeRegressor(random_state=config.RANDOM_SEED),
-        "ada": AdaBoostRegressor(random_state=config.RANDOM_SEED),
-        "rf": _rf_classifier,
-        "xgb": xgb.XGBRegressor(),
-    }
+class Model(BaseEstimator, RegressorMixin):
+    name: str
+    # TODO: Add correct typehint
+    base_model: Any
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, base_model: Any):
         self.name = name
-        self.predictor = self.all_predictors[name]
+        self.base_model = base_model
 
-        self._rf_classifier.estimator_ = DecisionTreeRegressor(
-            random_state=config.RANDOM_SEED
-        )
+    def fit(self, X, y):
+        self.base_model.fit(X, y)
+        return self
+
+    def predict(self, X):
+        return self.base_model.predict(X)
+
+    def score(self, X, y):
+        return self.base_model.score(X, y)
+
+
+_rf_classifier = RandomForestRegressor(n_jobs=-1, random_state=config.RANDOM_SEED)
+MODELS: Dict[str, Model] = {
+    "dummy_mean": Model("dummy_mean", DummyRegressor(strategy="mean")),
+    "dummy_median": Model("dummy_median", DummyRegressor(strategy="median")),
+    "ridge": Model("ridge", Ridge(random_state=config.RANDOM_SEED)),
+    # "svr": Model("svr", SVR()),
+    "knn": Model("knn", KNeighborsRegressor(n_jobs=-1)),
+    "dt": Model("dt", DecisionTreeRegressor(random_state=config.RANDOM_SEED)),
+    "ada": Model("ada", AdaBoostRegressor(random_state=config.RANDOM_SEED)),
+    "rf": Model("rf", _rf_classifier),
+    "xgb": xgb.XGBRegressor(),
+}
+_rf_classifier.estimator_ = DecisionTreeRegressor(random_state=config.RANDOM_SEED)
 
 
 class Metric:
@@ -78,14 +92,55 @@ class Evaluator:
         y_train: Dataset,
         X_test: Dataset,
         y_test: Dataset,
-    ):
+    ) -> Dict[str, float]:
         y_train_pred = pipeline.predict(X_train)
         y_test_pred = pipeline.predict(X_test)
 
-        train_metric = self.metric.compute(y_train.values, y_train_pred)
-        test_metric = self.metric.compute(y_test.values, y_test_pred)
+        train_score = self.metric.compute(y_train.values, y_train_pred)
+        test_score = self.metric.compute(y_test.values, y_test_pred)
 
-        return {"train": train_metric, "test": test_metric}
+        scores: Dict[str, float] = {"train": train_score, "test": test_score}
+        return scores
+
+
+def train_and_evaluate(
+    pipeline: Pipeline, df_train: Dataset, df_test: Dataset
+) -> Dict[str, float]:
+    X_train = get_X_set(df_train)
+    X_test = get_X_set(df_test)
+    y_train = get_y_set(df_train)
+    y_test = get_y_set(df_test)
+
+    model: Model = pipeline[-1]
+
+    # Train
+    logging.info(f"Training predictor {model.name}...")
+    pipeline.fit(X_train, y_train.values)
+    logging.info(f"Trained predictor {model.name} successfully.")
+
+    # Evaluate
+    evaluator = Evaluator(METRICS["r2"])
+    logging.info(f"Evaluating predictor {model.name}.")
+    metrics = evaluator.start(pipeline, X_train, y_train, X_test, y_test)
+    logging.info(f"Evaluated predictor {model.name}.")
+
+    return metrics
+
+
+class Runner:
+    def __init__(self, models: list[Model]):
+        self.models = models
+
+    def start(self, pipeline: Pipeline, df_train: Dataset, df_test: Dataset):
+        pipeline = copy.deepcopy(pipeline)
+
+        for model in self.models:
+            main_pipe = copy.deepcopy(pipeline)
+            main_pipe.steps.append(("predictor", model))
+            scores = train_and_evaluate(main_pipe, df_train, df_test)
+
+            logging.info(f"Train score for {model.name}: {scores['train']}")
+            logging.info(f"Test score for {model.name}: {scores['test']}")
 
 
 def main():
