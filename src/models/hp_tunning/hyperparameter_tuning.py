cc0b1b2c8c99879d3fb205343d5c7241ac10a343
aleksa-radojicic <aleksa.radojicic@outlook.com>
2024-03-20 19:03:47 +0100

    refactor: Increase cohesion of hp_tuning/main.py by introducing HPRunner class
    HPRunner is responsible for acquiring processed data and further tailoring it so it can proceed with tuning hyperparameters.

--- src/models/hp_tunning/hyperparameter_tuning.py
+++ src/models/hp_tunning/hyperparameter_tuning.py
@@ -2,13 +2,17 @@ import os
 from dataclasses import dataclass
 from typing import Any
 
+from omegaconf.errors import ConfigKeyError
+from omegaconf.omegaconf import DictConfig
 from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.model_selection._search import BaseSearchCV
 from sklearn.pipeline import Pipeline
 
+from src.features.build_features import FeaturesBuilder
 from src.logger import log_message
 from src.models.train.train_model import Metric, Model, deserialize_base_model
-from src.utils import Dataset
+from src.utils import (Dataset, add_prefix, get_X_set, get_y_set, load_data,
+                       train_test_split_custom)
 
 
 @dataclass
@@ -70,3 +74,114 @@ def get_base_model(model_type: str, model_dir: str) -> Model:
     model_name: str = model_type.split(".", 1)[0]
     model = Model(name=model_name, base_model=base_model)
     return model
+
+
+@dataclass
+class HPRunnerConfig:
+    data_filepath: str
+    base_model_filepath: str
+
+    test_size: float
+    random_seed: int
+    label_col: str
+
+    model_type: str
+    metric: str
+
+    pipeline_step_names: list[str]
+    preprocessor_name: str
+    predictor_name: str
+
+    hyperparameter_tuning: HPTunerConfig
+
+
+class HPRunner:
+    """Class responsible for loading processed data and further preparing it. After all it performs
+    tuning hyperparameters using sklearn's GridSearchCV.
+
+    Attributes
+    -------
+    cfg
+        Configuration containig all necessary informations for the HPRunner object.
+    """
+
+    cfg: HPRunnerConfig
+
+    def __init__(self, cfg: HPRunnerConfig):
+        self.cfg = cfg
+
+    def create_param_grid(self) -> dict[str, Any]:
+        """Create parameter grid for hyperparameter tuner such
+        that all hyperparameters have their appropriate prefix.
+
+        Prefixes are added to hyperparameters for preprocessor pipeline
+        containing various custom transformers and for the model (predictor).
+        """
+
+        hyperparams: DictConfig = self.cfg.hyperparameter_tuning.param_grid  # type: ignore
+        separator = "__"
+
+        # Preprocessor param grid
+        preprocessor_param_grid: dict[str, Any] = {}
+
+        for step_name in self.cfg.pipeline_step_names:
+            try:
+                step_name_prefix = (
+                    f"{self.cfg.preprocessor_name}{separator}{step_name}{separator}"
+                )
+                step_param_grid = add_prefix(
+                    prefix=step_name_prefix, **hyperparams[step_name]
+                )
+                preprocessor_param_grid = {**preprocessor_param_grid, **step_param_grid}
+            # If certain hyperparameter is not set in the config file,
+            # it won't raise an error.
+            except ConfigKeyError as e:
+                pass
+
+        # Model param grid
+        predictor_prefix = f"{self.cfg.predictor_name}{separator}"
+        base_model_prefix = f"base_model{separator}"
+        model_prefix = f"{predictor_prefix}{base_model_prefix}"
+
+        model_param_grid = add_prefix(prefix=model_prefix, **dict(hyperparams.model))
+
+        param_grid = {**preprocessor_param_grid, **model_param_grid}
+        return param_grid
+
+    def start(self) -> HyperparametersTuner:
+        cfg = self.cfg
+        df_processed, metadata_processed = load_data(cfg.data_filepath)
+
+        df_train, _ = train_test_split_custom(
+            df=df_processed, test_size=cfg.test_size, random_seed=cfg.random_seed
+        )
+        X_train = get_X_set(df_train, label_col=cfg.label_col)
+        y_train = get_y_set(df_train, label_col=cfg.label_col)
+
+        hp_tuning_cfg = cfg.hyperparameter_tuning
+        metric = Metric.from_name(cfg.metric)
+
+        features_builder = FeaturesBuilder()
+        preprocess_pipe = features_builder.make_pipeline(
+            cfg.pipeline_step_names, metadata_processed
+        )
+        model = get_base_model(
+            model_type=cfg.model_type, model_dir=cfg.base_model_filepath
+        )
+
+        pipeline = Pipeline(
+            [
+                (cfg.preprocessor_name, preprocess_pipe),
+                (cfg.predictor_name, model),
+            ]
+        )
+
+        param_grid = self.create_param_grid()
+
+        hyperparameter_tuner: HyperparametersTuner = HyperparametersTuner(
+            cfg=hp_tuning_cfg, metric=metric
+        )
+        hyperparameter_tuner.start(
+            estimator=pipeline, param_grid=param_grid, X=X_train, y=y_train
+        )
+        return hyperparameter_tuner
--- src/models/hp_tunning/main.py
+++ src/models/hp_tunning/main.py
@@ -1,100 +1,19 @@
-from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
 import hydra
 from hydra.core.config_store import ConfigStore
-from omegaconf import open_dict
-from omegaconf.errors import ConfigKeyError
-from sklearn.pipeline import Pipeline
 
-from src.features.build_features import FeaturesBuilder
-from src.models.hp_tunning.hyperparameter_tuning import (HPTunerConfig,
-                                                         HyperparametersTuner,
-                                                         get_base_model)
-from src.models.train.train_model import Metric, Model
-from src.utils import (Dataset, add_prefix, get_X_set, get_y_set, load_data,
-                       train_test_split_custom)
+from src.models.hp_tunning.hyperparameter_tuning import (HPRunner,
+                                                         HPRunnerConfig)
 
 # NOTE: Could be used as arguments in main and parsed
 CONFIG_PATH: str = str(Path().absolute() / "config" / "hyperparameters_tuning")
-BASE_MODEL_PATH: str = str(Path().absolute() / "models" / "base")
 CONFIG_FILE_NAME: str = "hyperparameters_tuning"
 HYDRA_VERSION_BASE: str = "1.3.1"  # NOTE: Could be in config
-PIPELINE_STEP_NAMES: list[str] = [
-    "ua_clean",
-    "ma_clean",
-    "col_dropper",
-    "cat_handler",
-    "nan_handler",
-    "final_ct",
-]
-PREPROCESSOR_NAME: str = "preprocessor"
-PREDICTOR_NAME: str = "predictor"
-
-
-@dataclass
-class HPConfig:
-    data_filepath: str
-    test_size: float
-    random_seed: int
-    label_col: str
-
-    model_type: str
-    metric: str
-
-    hyperparameter_tuning: HPTunerConfig
-
 
 cs: ConfigStore = ConfigStore.instance()
-cs.store(name="hp_tuning", node=HPConfig)
-
-
-def setup_metric(cfg):
-    # NOTE: cfg is of type DictConfig
-    with open_dict(cfg):
-        metric_name: str = cfg.metric
-        metric: Metric = Metric.from_name(metric_name)
-        cfg.metric = metric
-
-
-def create_param_grid(
-    preprocessor_name: str,
-    predictor_name: str,
-    pipe_step_names: list[str],
-    hyperparams: Any,
-) -> dict[str, Any]:
-    separator = "__"
-
-    # Preprocessor param grid
-    premodel_param_grid: dict[str, Any] = {}
-
-    for step_name in pipe_step_names:
-        try:
-            step_name_prefix = f"{preprocessor_name}{separator}{step_name}{separator}"
-            step_param_grid = add_prefix(
-                prefix=step_name_prefix, **hyperparams[step_name]
-            )
-            premodel_param_grid = {**premodel_param_grid, **step_param_grid}
-        # If certain hyperparameter is not set in the config file,
-        # it won't raise an error.
-        except ConfigKeyError as e:
-            pass
-
-    # Model param grid
-    predictor_prefix: str = f"{predictor_name}{separator}"
-    base_model_prefix: str = f"base_model{separator}"
-    model_prefix: str = f"{predictor_prefix}{base_model_prefix}"
-
-    model_param_grid: dict[str, Any] = add_prefix(
-        prefix=model_prefix, **dict(hyperparams.model)
-    )
-
-    param_grid: dict[str, Any] = {
-        **premodel_param_grid,
-        **model_param_grid,
-    }
-    return param_grid
+cs.store(name="hp_tuning", node=HPRunnerConfig)
 
 
 @hydra.main(
@@ -102,39 +21,9 @@ def create_param_grid(
     config_name=CONFIG_FILE_NAME,
     version_base=HYDRA_VERSION_BASE,
 )
-def main(cfg: HPConfig):
-    df_processed, metadata_processed = load_data(cfg.data_filepath)
-
-    df_train, _ = train_test_split_custom(
-        df=df_processed, test_size=cfg.test_size, random_seed=cfg.random_seed
-    )
-    X_train: Dataset = get_X_set(df_train, label_col=cfg.label_col)
-    y_train: Dataset = get_y_set(df_train, label_col=cfg.label_col)
-
-    hp_tuning_cfg: HPTunerConfig = cfg.hyperparameter_tuning
-    metric: Metric = Metric.from_name(cfg.metric)
-
-    features_builder: FeaturesBuilder = FeaturesBuilder()
-    preprocess_pipe = features_builder.make_pipeline(
-        PIPELINE_STEP_NAMES, metadata_processed
-    )
-    model: Model = get_base_model(model_type=cfg.model_type, model_dir=BASE_MODEL_PATH)
-
-    pipeline = Pipeline([(PREPROCESSOR_NAME, preprocess_pipe), (PREDICTOR_NAME, model)])
-
-    param_grid: dict[str, Any] = create_param_grid(
-        PREPROCESSOR_NAME,
-        PREDICTOR_NAME,
-        PIPELINE_STEP_NAMES,
-        cfg.hyperparameter_tuning.param_grid,
-    )
-
-    hyperparameter_tuner: HyperparametersTuner = HyperparametersTuner(
-        cfg=hp_tuning_cfg, metric=metric
-    )
-    hyperparameter_tuner.start(
-        estimator=pipeline, param_grid=param_grid, X=X_train, y=y_train
-    )
+def main(cfg: HPRunnerConfig):
+    hp_runner = HPRunner(cfg)
+    hyperparameter_tuner = hp_runner.start()
 
     cv_results: dict[str, Any] = hyperparameter_tuner.tuner.cv_results_  # type: ignore
     print(cv_results["mean_train_score"])
